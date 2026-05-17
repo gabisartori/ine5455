046920a0f838f84b229af98af6ca66752839f71a
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2026-05-17 19:38:12 -0300

    Teste 5: Implementa método de atribuição do projeto a um funcionário, teste passou

--- aulas/aula-7/src/employee.py
+++ aulas/aula-7/src/employee.py
@@ -1,6 +1,13 @@
 class Employee:
   def __init__(self, name: str):
     self.name: str = name
-  
+    self.projects: list = []
+
+  def add_project(self, project):
+    self.projects.append(project)
+
   def get_name(self) -> str:
     return self.name
+
+  def get_projects(self) -> list:
+    return self.projects
--- aulas/aula-7/src/project.py
+++ aulas/aula-7/src/project.py
@@ -2,9 +2,17 @@ class Project:
   def __init__(self, title: str):
     self.title: str = title
     self.company: str = None
-  
+    self.employees: list = []
+
+  def add_employee(self, employee):
+    self.employees.append(employee)
+    employee.add_project(self)
+
   def get_title(self) -> str:
     return self.title
 
   def get_company(self) -> str:
     return self.company
+  
+  def get_employees(self) -> list:
+    return self.employees
