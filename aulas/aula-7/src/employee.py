774d8e1f97b03e0447f041d05bc8424e90a3c286
gabisartori <gabriel.sartorirangel@gmail.com>
2026-05-19 09:29:01 -0300

    Teste 14: Implementa atribuição de ocorrência a funcionário

--- aulas/aula-7/src/employee.py
+++ aulas/aula-7/src/employee.py
@@ -3,12 +3,19 @@ class Employee:
     self.name: str = name
     self.projects: list = []
     self.company = None
+    self.tasks = []
 
   def add_project(self, project):
     self.projects.append(project)
+
+  def add_task(self, task):
+    self.tasks.append(task)
 
   def get_name(self) -> str:
     return self.name
 
   def get_projects(self) -> list:
     return self.projects
+  
+  def get_tasks(self) -> list:
+    return self.tasks
--- aulas/aula-7/src/task.py
+++ aulas/aula-7/src/task.py
@@ -7,9 +7,17 @@ class Task:
   def __init__(self, title: str):
     self.title: str = title
     self.status: TaskStatus = TaskStatus.OPEN
+    self.owner = None
+
+  def assign_to(self, employee):
+    employee.add_task(self)
+    self.owner = employee
 
   def get_title(self) -> str:
     return self.title
   
-  def get_status(self) -> TaskStatus.OPEN:
+  def get_status(self) -> TaskStatus:
     return self.status
+  
+  def get_owner(self):
+    return self.owner
