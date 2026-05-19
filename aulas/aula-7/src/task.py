c02f3c65e4f49066b08b0cbf8f1768b2d3a0f9e6
gabisartori <gabriel.sartorirangel@gmail.com>
2026-05-19 09:09:00 -0300

    Teste 12: Implementa status na ocorrência e inicializa o status em aberto, teste passou

--- aulas/aula-7/src/task.py
+++ aulas/aula-7/src/task.py
@@ -1,6 +1,15 @@
+from enum import Enum
+class TaskStatus(Enum):
+  OPEN = 0
+  CLOSED = 1
+
 class Task:
   def __init__(self, title: str):
     self.title: str = title
+    self.status: TaskStatus = TaskStatus.OPEN
 
   def get_title(self) -> str:
     return self.title
+  
+  def get_status(self) -> TaskStatus.OPEN:
+    return self.status
