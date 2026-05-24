cd0d1828aedea4e04369bb7bb8df5d52195e6745
gabisartori <gabriel.sartorirangel@gmail.com>
2026-05-24 19:52:05 -0300

    Teste 18: Tenta mudar prioridade de ocorrência fechada

--- aulas/aula-7/tests/test_tasks.py
+++ aulas/aula-7/tests/test_tasks.py
@@ -13,3 +13,9 @@ class TestTaks(unittest.TestCase):
     minor_to_major_task = Task("Coisa que eu não dei muita bola de cara", TaskPriority.LOW)
     minor_to_major_task.set_priority(TaskPriority.MEDIAN)
     self.assertEqual(TaskPriority.MEDIAN, minor_to_major_task.priority)
+
+  def test_change_closed_task_priority(self):
+    minor_to_major_task = Task("Coisa que eu terminei antes de perceber que era mais importante", TaskPriority.LOW)
+    minor_to_major_task.close()
+    with self.assertRaises(ValueError):
+      minor_to_major_task.set_priority(TaskPriority.HIGH)
