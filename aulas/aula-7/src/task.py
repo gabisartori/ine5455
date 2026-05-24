88504622125fc77fbaff79f765d3dd180719e037
gabisartori <gabriel.sartorirangel@gmail.com>
2026-05-24 19:43:58 -0300

    Teste 16: Implementa comparação entre prioridades de ocorrências, teste passou

--- aulas/aula-7/src/task.py
+++ aulas/aula-7/src/task.py
@@ -3,12 +3,18 @@ class TaskStatus(Enum):
   OPEN = 0
   CLOSED = 1
 
+class TaskPriority(Enum):
+  LOW = 0
+  MEDIAN = 1
+  HIGH = 2
+
 class Task:
-  def __init__(self, title: str):
+  def __init__(self, title: str, priority: TaskPriority):
     self.title: str = title
     self.status: TaskStatus = TaskStatus.OPEN
     self.owner = None
     self.project = None
+    self.priority = priority
 
   def assign_to(self, employee):
     if employee not in self.project.get_employees():
@@ -24,3 +30,6 @@ class Task:
   
   def get_owner(self):
     return self.owner
+  
+  def __gt__(self, other):
+    return self.priority.value > other.priority.value
