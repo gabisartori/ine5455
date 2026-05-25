5d174fb15ea80780b764caa6e381da5863516b58
gabisartori <gabriel.sartorirangel@gmail.com>
2026-05-24 23:19:44 -0300

    Teste 22: Adapta testes antigos ao novo método de criação de ocorrência

--- aulas/aula-7/tests/test_creations.py
+++ aulas/aula-7/tests/test_creations.py
@@ -2,7 +2,7 @@ import unittest
 from src.company import Company
 from src.employee import Employee
 from src.project import Project
-from src.task import Task, TaskStatus
+from src.task import Task, TaskStatus, TaskPriority
 
 class TestCreation(unittest.TestCase):
   def test_create_company(self):
@@ -18,9 +18,9 @@ class TestCreation(unittest.TestCase):
     self.assertEqual("Site de inscrição de alunos", project.get_title())
 
   def test_create_task(self):
-    task = Task("Corrigir bug na página inicial")
+    task = Task("Corrigir bug na página inicial", TaskPriority.MEDIAN)
     self.assertEqual("Corrigir bug na página inicial", task.get_title())
   
   def test_task_starts_with_open_status(self):
-    task = Task("Corrigir bug na página incial")
+    task = Task("Corrigir bug na página incial", TaskPriority.MEDIAN)
     self.assertEqual(TaskStatus.OPEN, task.get_status())
